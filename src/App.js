7adb50bac82677c30280946b44888fd558ebb554
ddelfaus <ddelfaus@gmail.com>
2019-10-07 16:45:40 -0400

    Stretch Goals

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import Timer from './Timer.js';
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeCount, setHomeCount] = useState(0);
   const [awayCount, setAwayCount] = useState(0);
+  const [quarterCount,setQuarterCount]= useState(1);
 
 
   return (
+
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
@@ -19,11 +22,16 @@ function App() {
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
             <div className="home__score">{homeCount}</div>
+            <h2 class ="away_name">Quater</h2>
+            <div className="away_score">{quarterCount}</div>
           </div>
-          <div className="timer">00:03</div>
+          <Timer />
+          {/* <div className="timer">00:03</div> */}
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayCount}</div>
+            <h2 class ="away_name">Quater</h2>
+            <div className="away_score">{quarterCount}</div>
           </div>
         </div>
         <BottomRow />
@@ -37,7 +45,9 @@ function App() {
         <div className="awayButtons">
           <button className="awayButtons__touchdown"onClick={() => setAwayCount(awayCount + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal"onClick={() => setAwayCount(awayCount + 3)}>Away Field Goal</button>
+          <button className="awayButtons__fieldGoal"onClick={() => setQuarterCount(quarterCount + 1)}>Quater</button>
         </div>
+        <button className ="buttons" onClick={() =>(setHomeCount(0),setAwayCount(0),setQuarterCount(1))}>Reset</button>
       </section>
     </div>
   );
